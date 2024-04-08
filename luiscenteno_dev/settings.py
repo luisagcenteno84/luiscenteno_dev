f98b345500734e84a96d8c60b69aa3e0664baaad
luisagcenteno84 <luisagcenteno@gmail.com>
2024-04-07 21:11:10 -0700

    adding dir for templates

--- luiscenteno_dev/settings.py
+++ luiscenteno_dev/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'luiscenteno_dev.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / "luiscenteno_dev/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
