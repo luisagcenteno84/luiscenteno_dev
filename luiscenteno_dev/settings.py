4e6d74ff11511ce30a1ed62a9303646626f56856
luisagcenteno84 <luisagcenteno@gmail.com>
2024-12-30 23:30:27 -0700

    turning debug off

--- luiscenteno_dev/settings.py
+++ luiscenteno_dev/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7ykoy$b5^vvhms+l9&udel&-b0u%$%hgnpzk*!xpva2w2$culr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
